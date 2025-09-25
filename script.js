bcd864deda4679145e7faade19701319d4a165df
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2025-09-25 14:23:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+class Product {
+  constructor(name, cost, expense) {
+    this.stock = stock;
+    this.name = name;
+    this.cost = cost;
+    this.expense = expense;
+  }
+}
+
+class Customer {
+  constructor(name, want) {
+    this.name = name;
+    this.want = want;
+  }
+}
+
+
+// disable invest product button if not invested
+
+// deliver product buttons
