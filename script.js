af5cd77f92d308c2219320fc4d2ec1b3cda86f34
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2025-09-26 06:42:54 +0200

    add book stock and total cash, also can deliver book with updating numbers

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const investAmount = 5;
+let totalcash = 20;
+
+const totalCashDisplay = document.getElementById("totalcash");
+const bookStock = document.getElementById("bookstock");
+
+totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
+
+///////////////////////////////////////////////
+
 class Product {
-  constructor(name, cost, expense) {
-    this.stock = stock;
+  constructor(name, cost, expense, price) {
     this.name = name;
     this.cost = cost;
     this.expense = expense;
+    this.price = price;
   }
+  stock = investAmount;
 }
 
 class Customer {
@@ -14,6 +25,47 @@ class Customer {
   }
 }
 
+let book = new Product("Book", 17, 10, 5);
+
+/////////////////////////////////////////////
+
+const btn1 = document.getElementById("btn1");
+const bookdeliver = document.createElement("button");
+bookdeliver.innerText = "Deliver Book";
+bookdeliver.id = "btn11";
+const bookdeliversection = document.getElementById("book");
+
+book.stock = 0;
+function investBook() {
+  if (totalcash >= 0 && totalcash >= book.cost) {
+    bookStock.innerHTML = ` Book Stock :  ${(book.stock =
+      book.stock + investAmount)}`;
+    bookdeliversection.appendChild(bookdeliver);
+    totalcash -= book.cost;
+    totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
+  } else {
+    console.log("baby no money to invest in something!!");
+  }
+}
+
+btn1.addEventListener("click", investBook);
+
+////////////////////////////////////////////////
+
+const btn11 = document.getElementById("btn11");
+
+function deliverBook() {
+  if (book.stock >= 0) {
+    bookStock.innerHTML = ` Book Stock :  ${book.stock--}`;
+    totalcash += book.price;
+    totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
+  } else {
+    console.log("no book in stock");
+  }
+}
+bookdeliver.addEventListener("click", deliverBook);
+
+function randomCustomer() {}
 
 // disable invest product button if not invested
 
