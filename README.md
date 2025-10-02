37d1f7699be3b24780a65c45a7dab57bbf76665e
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2025-10-02 11:41:36 +0200

    invest in lemonade and book, also have 2 customers randomized

--- README.md
+++ README.md
@@ -2,5 +2,19 @@
 
 ### Mini Business Simulation Game
 
+## Last what I do
 
 
+
+
+
+
+
+
+
+
+## What I plan to do
+
+* recursive setTimeout function could be used to make it similar to real life 
+  - in regular monthly financial timeframes (40 secs)
+  - randomized customers like in real life(3, 7, 11 secs)
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const investAmount = 5;
 let totalcash = 20;
+let investAmount = 0;
 
 const totalCashDisplay = document.getElementById("totalcash");
 const bookStock = document.getElementById("bookstock");
+const lemonadeStock = document.getElementById("lemonadestock");
+const warningSection = document.getElementById("warning");
 
 totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
 
@@ -25,47 +27,133 @@ class Customer {
   }
 }
 
-let book = new Product("Book", 17, 10, 5);
+
+  const products = [
+    new Product("Book", 17, 10, 5),
+    new Product("Lemonade", 10, 2, 4),
+  ];
+  const productRandomIdx = Math.floor(Math.random() * products.length);
+  let customers = [
+    new Customer("Maksym", products[productRandomIdx]),
+    new Customer("Vitalii", products[productRandomIdx]),
+  ];
+  const customerRandomIdx = Math.floor(Math.random() * customers.length);
+
 
 /////////////////////////////////////////////
-
 const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 const bookdeliver = document.createElement("button");
-bookdeliver.innerText = "Deliver Book";
-bookdeliver.id = "btn11";
-const bookdeliversection = document.getElementById("book");
+const lemonadedeliver = document.createElement("button");
 
-book.stock = 0;
-function investBook() {
-  if (totalcash >= 0 && totalcash >= book.cost) {
-    bookStock.innerHTML = ` Book Stock :  ${(book.stock =
-      book.stock + investAmount)}`;
+function investInBook(productIdx) {
+  bookdeliver.id = "btn11";
+
+  const bookdeliversection = document.getElementById("book");
+
+  const investAmount = 5;
+  if (totalcash >= 0 && totalcash >= products[productIdx].cost) {
+    bookStock.innerHTML = ` ${products[productIdx].name} Stock :  ${(products[
+      productIdx
+    ].stock = products[productIdx].stock + investAmount)}`;
+    bookdeliver.innerText = `Deliver ${products[0].name} ${products[0].price}$`;
     bookdeliversection.appendChild(bookdeliver);
-    totalcash -= book.cost;
+
+    totalcash -= products[productIdx].cost;
     totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
   } else {
-    console.log("baby no money to invest in something!!");
+    warningSection.innerText = `baby not enough money(${totalcash}$) to invest in something!!`;
+    setTimeout(() => {
+      warningSection.innerText = "";
+    }, 2000);
   }
 }
 
-btn1.addEventListener("click", investBook);
+function investInLemonade(productIdx) {
+  lemonadedeliver.id = "btn21";
+
+  const lemonadedeliversection = document.getElementById("lemonade");
+  const investAmount = 5;
+  if (totalcash >= 0 && totalcash >= products[productIdx].cost) {
+    lemonadeStock.innerHTML = ` ${
+      products[productIdx].name
+    } Stock :  ${(products[productIdx].stock =
+      products[productIdx].stock + investAmount)}`;
+
+    lemonadedeliver.innerText = `Deliver ${products[1].name} ${products[1].price}$`;
+    lemonadedeliversection.appendChild(lemonadedeliver);
+    totalcash -= products[productIdx].cost;
+    totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
+  } else {
+    warningSection.innerText = `baby not enough money(${totalcash}$) to invest in something!!`;
+    setTimeout(() => {
+      warningSection.innerText = "";
+    }, 2000);
+  }
+}
+btn1.addEventListener("click", () => investInBook(0));
+btn2.addEventListener("click", () => investInLemonade(1));
 
 ////////////////////////////////////////////////
 
 const btn11 = document.getElementById("btn11");
+const btn21 = document.getElementById("btn21");
 
 function deliverBook() {
-  if (book.stock >= 0) {
-    bookStock.innerHTML = ` Book Stock :  ${book.stock--}`;
-    totalcash += book.price;
+  if (products[0].stock > 0) {
+    products[0].stock--;
+    bookStock.innerHTML = ` ${products[0].name} Stock : ${products[0].stock} `;
+    totalcash += products[0].price;
     totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
   } else {
-    console.log("no book in stock");
+    warningSection.innerText = `not enough product(${
+      products[0].stock + " " + products[0].name
+    }) in stock`;
+    setTimeout(() => {
+      warningSection.innerText = "";
+    }, 2000);
   }
 }
-bookdeliver.addEventListener("click", deliverBook);
 
-function randomCustomer() {}
+function deliverLemonade() {
+  if (products[1].stock > 0) {
+    products[1].stock--;
+    lemonadeStock.innerHTML = ` ${products[1].name} Stock : ${products[1].stock} `;
+    totalcash += products[1].price;
+    totalCashDisplay.innerHTML = `Total:  ${totalcash} `;
+  } else {
+    warningSection.innerText = `not enough product(${
+      products[1].stock + " " + products[1].name
+    }) in stock`;
+    setTimeout(() => {
+      warningSection.innerText = "";
+    }, 2000);
+  }
+}
+
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randomCustomer() {
+
+const deliver = document.getElementById("deliver");
+  const randomCustomer = getRandomItem(customers);
+  const randomProduct = getRandomItem(products);
+  if (randomCustomer && randomProduct) {
+    deliver.innerHTML = `<br>${randomCustomer.name} wants a ${randomProduct.name}<br>`;
+    
+    bookdeliver.addEventListener("click", deliverBook);
+    lemonadedeliver.addEventListener("click", deliverLemonade);
+    return { customer: randomCustomer, want: randomProduct };
+  } else {
+    deliver.innerText = `no customer yet`;
+    return null;
+  }
+}
+setInterval(() => {
+  randomCustomer();
+}, 9000);
 
 // disable invest product button if not invested
 
